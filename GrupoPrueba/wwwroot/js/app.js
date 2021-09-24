b73c465e49c64361ca5a0ef9c437ecf0440eef0d
Diego Chiroque <diego-al-18@hotmail.com>
2021-09-23 23:03:47 -0500

    error

---
+++ GrupoPrueba/wwwroot/js/app.js
@@ -0,0 +1,10 @@
+    let btncategoria=document.querySelector('.cat');
+    
+
+    btncategoria.addEventListener('click',()=>{
+            document.querySelector('.categoria').classList.toggle('active');
+    })
+
+
+  
+   
